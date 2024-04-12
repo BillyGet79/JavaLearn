f2c81a39cd78676d797745e75171d207d718e781
gaoyifei <2909626219@qq.com>
2024-04-12 12:07:49 +0800

    算法学习

--- algorithmBrushing/src/class01/Code06_AOE.java
+++ algorithmBrushing/src/class01/Code06_AOE.java
@@ -1,5 +1,7 @@
 package class01;
 
+import java.util.Arrays;
+
 /**
  * 题目6
  * 给定两个非负数组x和hp，长度都是N，再给定一个正数range
@@ -15,5 +17,230 @@ public class Code06_AOE {
     //然后我们向右移动AOE范围，使得边缘范围上的怪兽血量值为正数，接着这样操作
     //这样我们就能解决这个问题
     //不过对于区间范围内元素的整体加减，我们需要考虑到线段树，使用线段树的方式来优化区间整体数字的加减
+
+
+    //先只使用贪心策略来解决
+    public static int minAoe2(int[] x, int[] hp, int range) {
+        int N = x.length;
+        int ans = 0;
+        //遍历i，获取x[i]和hp[i]
+        for (int i = 0; i < N; i++) {
+            if (hp[i] > 0) {
+                //找到range范围内的右侧怪兽下标
+                int triggerPost = i;
+                while (triggerPost < N && x[triggerPost] - x[i] <= range) {
+                    triggerPost++;
+                }
+                ans += hp[i];
+                //进行aoe掉血操作
+                aoe(x, hp, i, triggerPost - 1, range);
+            }
+        }
+        return ans;
+    }
+
+    //aoe掉血操作
+    public static void aoe(int[] x, int[] hp, int L, int trigger, int range) {
+        int N = x.length;
+        int RPost = trigger;
+        //找到范围内的右边界
+        while (RPost < N && x[RPost] - x[trigger] <= range) {
+            RPost++;
+        }
+        //找到要aoe减掉的血量
+        int minus = hp[L];
+        for (int i = L; i < RPost; i++) {
+            hp[i] = Math.max(0, hp[i] - minus);
+        }
+    }
+
+    //引入线段树来进行解决问题，这样算法能够优化成O(N*logN)
+    public static int minAoe3(int[] x, int[] hp, int range) {
+        int N = x.length;
+        //coverLeft[i]：如果以i为中心点放技能，左侧能影响到哪，下标从1开始，不从0开始
+        //coverRight[i]：如果以i为中心点放技能，右侧能影响到哪，下标从1开始，不从0开始
+        int[] coverLeft = new int[N + 1];
+        int[] coverRight = new int[N + 1];
+        int left = 0;
+        int right = 0;
+        for (int i = 0; i < N; i++) {
+            while (x[i] - x[left] > range) {
+                left++;
+            }
+            while (right < N && x[right] - x[i] <= range) {
+                right++;
+            }
+            coverLeft[i + 1] = left + 1;
+            coverRight[i + 1] = right;
+        }
+        //best[i]：如果i是最左边缘点，选哪个点做技能中心点最好，下标从1开始，不从0开始
+        int[] best = new int[N + 1];
+        int trigger = 0;
+        for (int i = 0; i < N; i++) {
+            while (trigger < N && x[trigger] - x[i] <= range) {
+                trigger++;
+            }
+            best[i + 1] = trigger;
+        }
+        SegmentTree st = new SegmentTree(hp);
+        st.build(1, N, 1);
+        int ans = 0;
+        for (int i = 1; i <= N; i++) {
+            long leftEdge = st.query(i, i, 1, N, 1);
+            if (leftEdge > 0) {
+                ans += leftEdge;
+                int t = best[i];
+                int l = coverLeft[t];
+                int r = coverRight[t];
+                st.add(l, r, (int)(-leftEdge), 1, N, 1);
+            }
+        }
+        return ans;
+    }
+
+    public static class SegmentTree {
+        private int MAXN;
+        private int[] arr;
+        private int[] sum;
+        private int[] lazy;
+
+        public SegmentTree(int[] origin) {
+            MAXN = origin.length + 1;
+            arr = new int[MAXN];
+            for (int i = 1; i < MAXN; i++) {
+                arr[i] = origin[i - 1];
+            }
+            sum = new int[MAXN << 2];
+            lazy = new int[MAXN << 2];
+        }
+
+        private void pushUp(int rt) {
+            sum[rt] = sum[rt << 1] + sum[rt << 1 | 1];
+        }
+
+        private void pushDown(int rt, int ln, int rn) {
+            if (lazy[rt] != 0) {
+                lazy[rt << 1] += lazy[rt];
+                sum[rt << 1] += lazy[rt] * ln;
+                lazy[rt << 1 | 1] += lazy[rt];
+                sum[rt << 1 | 1] += lazy[rt] * rn;
+                lazy[rt] = 0;
+            }
+        }
+
+        private void build(int l, int r, int rt) {
+            if (l == r) {
+                sum[rt] = arr[l];
+                return;
+            }
+            int mid = (l + r) >> 1;
+            build(l, mid, rt << 1);
+            build(mid + 1, r, rt << 1 | 1);
+            pushUp(rt);
+        }
+
+        public void add(int L, int R, int C, int l, int r, int rt) {
+            if (L <= l && r <= R) {
+                sum[rt] += C * (r - l + 1);
+                lazy[rt] += C;
+                return;
+            }
+            int mid = (l + r) >> 1;
+            pushDown(rt, mid - l + 1, r - mid);
+            if (L <= mid) {
+                add(L, R, C, l, mid, rt << 1);
+            }
+            if (R > mid) {
+                add(L, R, C, mid + 1, r, rt << 1 | 1);
+            }
+            pushUp(rt);
+        }
+
+        public int query(int L, int R, int l, int r, int rt) {
+            if (L <= l && r <= R) {
+                return sum[rt];
+            }
+            int mid = (l + r) >> 1;
+            pushDown(rt, mid - l + 1, r - mid);
+            int ans = 0;
+            if (L <= mid) {
+                ans += query(L, R, l, mid, rt << 1);
+            }
+            if (R > mid) {
+                ans += query(L, R, mid + 1, r, rt << 1 | 1);
+            }
+            return ans;
+        }
+    }
+
+    // 为了测试
+    public static int[] randomArray(int n, int valueMax) {
+        int[] ans = new int[n];
+        for (int i = 0; i < n; i++) {
+            ans[i] = (int) (Math.random() * valueMax) + 1;
+        }
+        return ans;
+    }
+
+    // 为了测试
+    public static int[] copyArray(int[] arr) {
+        int N = arr.length;
+        int[] ans = new int[N];
+        for (int i = 0; i < N; i++) {
+            ans[i] = arr[i];
+        }
+        return ans;
+    }
+
+    // 为了测试
+    public static void main(String[] args) {
+        int N = 50;
+        int X = 500;
+        int H = 60;
+        int R = 10;
+        int testTime = 50000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int len = (int) (Math.random() * N) + 1;
+            int[] x2 = randomArray(len, X);
+            Arrays.sort(x2);
+            int[] hp2 = randomArray(len, H);
+            int[] x3 = copyArray(x2);
+            int[] hp3 = copyArray(hp2);
+            int range = (int) (Math.random() * R) + 1;
+            int ans2 = minAoe2(x2, hp2, range);
+            int ans3 = minAoe3(x3, hp3, range);
+            if (ans2 != ans3) {
+                System.out.println("出错了！");
+            }
+        }
+        System.out.println("测试结束");
+
+        N = 500000;
+        long start;
+        long end;
+        int[] x2 = randomArray(N, N);
+        Arrays.sort(x2);
+        int[] hp2 = new int[N];
+        for (int i = 0; i < N; i++) {
+            hp2[i] = i * 5 + 10;
+        }
+        int[] x3 = copyArray(x2);
+        int[] hp3 = copyArray(hp2);
+        int range = 10000;
+
+        start = System.currentTimeMillis();
+        System.out.println(minAoe2(x2, hp2, range));
+        end = System.currentTimeMillis();
+        System.out.println("运行时间 : " + (end - start) + " 毫秒");
+
+        start = System.currentTimeMillis();
+        System.out.println(minAoe3(x3, hp3, range));
+        end = System.currentTimeMillis();
+        System.out.println("运行时间 : " + (end - start) + " 毫秒");
+    }
+
+
+
 
 }
---
+++ algorithmBrushing/src/class02/Code01_ChooseWork.java
@@ -0,0 +1,64 @@
+package class02;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.TreeMap;
+
+/**
+ * 题目1
+ * 给定数组hard和money，长度都为N
+ * hard[i]表示i号的难度，money[i]表示i号工作的收入
+ * 给定数组ability，长度都为M，ability[j]表示j号人的能力
+ * 每一号工作，都可以提供无数的岗位，难度和收入都一样
+ * 但是人的能力必须>=这份工作的难度，才能上班
+ * 返回一个长度为M的数组ans，ans[j]表示j号人能获得的最好收入
+ */
+public class Code01_ChooseWork {
+
+    //利用封装思想将hard和money封装起来，然后根据难度从小到大排序
+    //如果难度值相同，那么钱多的排前面
+    //我们考虑实际情况，因为每一份工作都可以有无数个人去工作，所以难度相同的情况下报酬少的工作可以不存在
+    //当然，如果某些工作难度大的情况下收入还比难度小的小，那么也可以不做保留
+    //这样我们最后留下来的工作难度和报酬都是递增的
+    //在这种情况下我们就可以遍历ability数组去寻找每个人能够获得的最大收入是多少
+
+    public static class Job {
+        public int hard;
+        public int money;
+
+        public Job(int hard, int money) {
+            this.hard = hard;
+            this.money = money;
+        }
+    }
+
+    public static int[] getMoneys(int[] hard, int[] money, int[] ability) {
+        int N = hard.length;
+        //封装
+        Job[] jobs = new Job[N];
+        for (int i = 0; i < N; i++) {
+            jobs[i] = new Job(hard[i], money[i]);
+        }
+        //排序
+        Arrays.sort(jobs, (o1, o2) -> o1.hard != o2.hard ? (o1.hard - o2.hard) : (o2.money - o1.money));
+        //因为我们要根据难度去重，最好的去重方式就是使用有序表去重
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(jobs[0].hard, jobs[0].money);
+        //通过pre保存上一个工作来确保报酬和难度都是递增的
+        Job pre = jobs[0];
+        for (int i = 1; i < N; i++) {
+            if (jobs[i].hard != pre.hard && jobs[i].money > pre.money) {
+                map.put(jobs[i].hard, jobs[i].money);
+                pre = jobs[i];
+            }
+        }
+        //在这种情况下，我们只需要根据难度获取工作即可
+        //可以直接使用有序表当中的floorKey方法，找到离ability[i]最近并且<=ability[i]的工作
+        int[] ans = new int[ability.length];
+        for (int i = 0; i < ability.length; i++) {
+            Integer key = map.floorKey(ability[i]);
+            ans[i] = key != null ? map.get(key) : 0;
+        }
+        return ans;
+    }
+}
