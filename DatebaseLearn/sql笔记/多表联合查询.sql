7758f660110e9c896017bda89ea694e4e10d2537
gaoyifei <2909626219@qq.com>
2024-04-14 15:40:00 +0800

    数据库练习

--- DatebaseLearn/sql笔记/多表联合查询.sql
+++ DatebaseLearn/sql笔记/多表联合查询.sql
@@ -80,6 +80,58 @@ SELECT * FROM emp3 WHERE age > ALL(SELECT age FROM emp3 WHERE dept_id = '1003');
 -- 查询不属于任何一个部门的员工信息
 SELECT * FROM emp3 WHERE dept_id != ALL(SELECT deptno FROM dept3);
 
+-- 子查询关键字-ANY和SOME
+
+-- 查询年龄大于'1003'部门任意一个员工年龄的员工信息
+SELECT * FROM emp3 WHERE age > SOME(SELECT age FROM emp3 WHERE dept_id = '1003') AND dept_id != '1003';
+
+-- 子查询关键字-IN
+
+-- 查询研发部和销售部的员工信息，包含员工号、员工名字
+SELECT eid, ename FROM emp3 WHERE dept_id IN (SELECT deptno FROM dept3 WHERE name = '研发部' OR name = '销售部');
+
+-- 子查询关键字-EXISTS
+
+-- 查询公司是否有大于60岁的员工，有则输出
+-- 这里一定要把两个emp3区分开来
+SELECT * FROM emp3 a WHERE EXISTS(SELECT * FROM emp3 b WHERE a.age > 60);
+
+-- 查询有所属部门的员工信息
+SELECT * FROM emp3 a WHERE EXISTS(SELECT * FROM dept3 b WHERE b.deptno = a.dept_id);
+
+-- 自关联查询
+-- 创建表,并建立自关联约束
+CREATE TABLE t_sanguo(
+    eid INT PRIMARY KEY ,
+    ename VARCHAR(20),
+    manager_id INT,
+		FOREIGN KEY (manager_id) REFERENCES t_sanguo (eid)  -- 添加自关联约束
+);
+
+-- 添加数据
+insert into t_sanguo values(1,'刘协',NULL);
+insert into t_sanguo values(2,'刘备',1);
+insert into t_sanguo values(3,'关羽',2);
+insert into t_sanguo values(4,'张飞',2);
+insert into t_sanguo values(5,'曹操',1);
+insert into t_sanguo values(6,'许褚',5);
+insert into t_sanguo values(7,'典韦',5);
+insert into t_sanguo values(8,'孙权',1);
+insert into t_sanguo values(9,'周瑜',8);
+insert into t_sanguo values(10,'鲁肃',8);
+
+-- 进行关联查询
+-- 1.查询每个三国人物及他的上级
+SELECT * FROM t_sanguo a, t_sanguo b WHERE a.manager_id = b.eid;
+SELECT a.ename, b.ename FROM t_sanguo a JOIN t_sanguo b ON a.manager_id = b.eid;
+
+-- 2.查询所有人物及上级
+SELECT a.ename, b.ename FROM t_sanguo a LEFT JOIN t_sanguo b on a.manager_id = b.eid;
+
+-- 3.查询所有人物、上级，上上级 
+SELECT a.ename, b.ename, c.ename FROM t_sanguo a LEFT JOIN t_sanguo b on a.manager_id = b.eid LEFT JOIN t_sanguo c ON b.manager_id = c.eid;
+
+
 
 
 
