2ec6ede3339c998bb5fd3145cf16159159100214
gaoyifei <2909626219@qq.com>
2024-04-18 09:15:36 +0800

    算法学习

--- algorithmBrushing/src/class03/Code01_LongestSubstringWithoutRepeatingCharacters.java
+++ algorithmBrushing/src/class03/Code01_LongestSubstringWithoutRepeatingCharacters.java
@@ -45,6 +45,42 @@ public class Code01_LongestSubstringWithoutRepeatingCharacters {
         return dp[0][N - 1];
     }
 
+    //不过涉及到字串问题，我们要有一个思维定式，即我们需要考虑以每个位置为结尾的子串
+    //这道题我们可以考虑以每个字母为结尾的子串最远不重复能推多远
+    //然后所有位置找最大即可
+    //所以我们要考虑每一个位置能推多远的决定因素是什么
+    //这里有两个决定因素：
+    //  1)当前字符上次的位置
+    //  2)前一个位置往左推的距离
+    //这两个位置，谁离当前位置近，以当前位置为结尾的无重复子串长度就是它
+    //不过每个位置我们只需要前一个位置的结果，所以我们不需要保存整张表（做空间压缩）
+
+    public static int lengthOfLongestSubstring1(String s) {
+        if (s == null || s.isEmpty()) {
+            return 0;
+        }
+        char[] str = s.toCharArray();
+        //因为ASCII码的范围是0~255，所以用一个256大小的数组足以表示所有的字符
+        //用map[i]表示i字符上次出现的位置
+        int[] map = new int[256];
+        for (int i = 0; i < 256; i++) {
+            map[i] = -1;
+        }
+        int N = str.length;
+        int ans = 1;
+        int pre = 1;    //上一个位置，向左推了多长
+        map[str[0]] = 0;
+        for (int i = 1; i < N; i++) {
+            int p1 = i - map[str[i]];
+            int p2 = pre + 1;
+            int cur = Math.min(p1, p2);
+            ans = Math.max(ans, cur);
+            pre = cur;
+            map[str[i]] = i;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) {
         String s = "abcabcbb";
         System.out.println(lengthOfLongestSubstring(s));
