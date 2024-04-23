fef7c2a3580520620f691110cffac1a8750e7f31
gaoyifei <2909626219@qq.com>
2024-04-23 11:20:19 +0800

    算法学习

--- algorithmBrushing/src/class04/Code07_InterleavingString.java
+++ algorithmBrushing/src/class04/Code07_InterleavingString.java
@@ -15,19 +15,52 @@ package class04;
 public class Code07_InterleavingString {
 
     /**
-     *
+     * 看到这道题我们会出现第一个想法，即设定三个指针，走指针对比
+     * 但是如果我们这么去做了，那就G了
+     * 比如1111kf和111tef组成1111kf111tef，你觉得是前后拼接，实际上是交错字符串（没想到吧hhhh）
+     * 这道题的正确解法是使用动态规划中的样本对应模型
+     * 我们定义dp[i][j]为str1拿前i个字符，str2拿前j个字符，是否能组成str3前i+j个字符
+     * 最后dp表的最右下角的位置的结果就是我们要的答案
      * @param s1
      * @param s2
      * @param s3
      * @return
      */
     public static boolean isInterleave(String s1, String s2, String s3) {
+        if (s1 == null || s2 == null || s3 == null) {
+            return false;
+        }
         char[] str1 = s1.toCharArray();
         char[] str2 = s2.toCharArray();
         char[] str3 = s3.toCharArray();
-        int index1 = 0;
-        int index2 = 0;
-        int index3 = 0;
-
+        if (str1.length + str2.length != str3.length) {
+            return false;
+        }
+        boolean[][] dp = new boolean[str1.length + 1][str2.length + 1];
+        //dp数组初始化
+        dp[0][0] = true;
+        //我们拿str1的第i个字符与str3的第i个字符比，如果相同，就初始化为true，直到不为true时break掉即可
+        for (int i = 1; i < str1.length + 1; i++) {
+            if (str1[i - 1] != str3[i - 1]) {
+                break;
+            }
+            dp[i][0] = true;
+        }
+        //与上面同理
+        for (int j = 1; j < str2.length + 1; j++) {
+            if (str2[j - 1] != str3[j - 1]) {
+                break;
+            }
+            dp[0][j] = true;
+        }
+        //开始dp
+        for (int i = 0; i < str1.length + 1; i++) {
+            for (int j = 0; j < str2.length + 1; j++) {
+                if (str1[i - 1] == str3[i + j - 1] && dp[i - 1][j] || str2[j - 1] == str3[i + j - 1] && dp[i][j - 1]) {
+                    dp[i][j] = true;
+                }
+            }
+        }
+        return dp[str1.length][str2.length];
     }
 }
