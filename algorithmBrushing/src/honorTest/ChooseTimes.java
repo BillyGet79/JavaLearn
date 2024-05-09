ccbd37ddd7706ea7f55e019b2cb814959d3267de
gaoyifei <2909626219@qq.com>
2024-05-09 21:12:24 +0800

    算法学习

---
+++ algorithmBrushing/src/honorTest/ChooseTimes.java
@@ -0,0 +1,40 @@
+package honorTest;
+
+import java.io.*;
+
+public class ChooseTimes {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StreamTokenizer in = new StreamTokenizer(br);
+        PrintWriter out = new PrintWriter(new OutputStreamWriter(System.out));
+
+        in.nextToken();
+        int rows = (int) in.nval;
+        int[][] arr = new int[rows][];
+        for (int i = 0; i < rows; i++) {
+            in.nextToken();
+            int length = (int) in.nval;
+            arr[i] = new int[length];
+            for (int j = 0; j < arr[i].length; j++) {
+                in.nextToken();
+                arr[i][j] = (int) in.nval;
+            }
+        }
+        for (int i = 0; i < arr.length; i++) {
+            out.println(countTimes(arr[i]));
+        }
+        out.flush();
+    }
+
+    public static int countTimes(int[] arr) {
+        int count = 0;
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if ((arr[i] + arr[j]) == (arr[i] ^ arr[j])) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
