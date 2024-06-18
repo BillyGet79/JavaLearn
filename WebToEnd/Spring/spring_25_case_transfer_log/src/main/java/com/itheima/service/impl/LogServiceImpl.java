dbd95f933f59184f9357e0a73e0c59d20ecf0aa2
gaoyifei <2909626219@qq.com>
2024-06-18 09:20:24 +0800

    Spring学习

---
+++ WebToEnd/Spring/spring_25_case_transfer_log/src/main/java/com/itheima/service/impl/LogServiceImpl.java
@@ -0,0 +1,18 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.LogDao;
+import com.itheima.service.LogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LogServiceImpl implements LogService {
+
+    @Autowired
+    private LogDao logDao;
+
+    @Override
+    public void log(String out, String in, Double money) {
+        logDao.log("转账操作由" + out + "到" + in + "，金额：" + money);
+    }
+}
