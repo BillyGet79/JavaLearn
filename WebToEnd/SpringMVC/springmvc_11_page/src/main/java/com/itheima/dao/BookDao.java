a837497f379e973ae1ef69f2cd7825f0a2307809
gaoyifei <2909626219@qq.com>
2024-07-05 21:10:57 +0800

    SpringMVC学习

--- WebToEnd/SpringMVC/springmvc_11_page/src/main/java/com/itheima/dao/BookDao.java
+++ WebToEnd/SpringMVC/springmvc_11_page/src/main/java/com/itheima/dao/BookDao.java
@@ -11,13 +11,13 @@ import java.util.List;
 public interface BookDao {
 
     @Insert("insert into tbl_book values(null, #{type}, #{name}, #{description})")
-    public void save(Book book);
+    public int save(Book book);
 
     @Update("update tbl_book set type = #{type}, name = #{name}, description = #{description} where id = #{id}")
-    public void update(Book book);
+    public int update(Book book);
 
     @Delete("delete from tbl_book where id = #{id}")
-    public void delete(Integer id);
+    public int delete(Integer id);
 
     @Select("select * from tbl_book where id = #{id}")
     public Book getById(Integer id);
