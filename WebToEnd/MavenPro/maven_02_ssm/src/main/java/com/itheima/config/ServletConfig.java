004fb4fb7eeb6ceb021df84742dfc1c262a42ff0
gaoyifei <2909626219@qq.com>
2024-07-09 20:36:46 +0800

    Maven高级学习

---
+++ WebToEnd/MavenPro/maven_02_ssm/src/main/java/com/itheima/config/ServletConfig.java
@@ -0,0 +1,31 @@
+package com.itheima.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class ServletConfig extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[]{SpringConfig.class};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[]{SpringMvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    //中文乱码问题
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter filter = new CharacterEncodingFilter();
+        filter.setEncoding("UTF-8");
+        return new Filter[]{filter};
+    }
+}
---
+++ WebToEnd/MavenPro/maven_02_ssm/src/main/java/com/itheima/service/impl/BookServiceImpl.java
@@ -0,0 +1,41 @@
+package com.itheima.service.impl;
+
+import com.itheima.dao.BookDao;
+import com.itheima.domain.Book;
+import com.itheima.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookServiceImpl implements BookService {
+
+    @Autowired
+    private BookDao bookDao;
+
+    @Override
+    public boolean save(Book book) {
+        return bookDao.save(book) > 0;
+    }
+
+    @Override
+    public boolean update(Book book) {
+        return bookDao.update(book) > 0;
+    }
+
+    @Override
+    public boolean delete(Integer id) {
+        return bookDao.delete(id) > 0;
+    }
+
+    @Override
+    public Book getById(Integer id) {
+        return bookDao.getById(id);
+    }
+
+    @Override
+    public List<Book> getAll() {
+        return bookDao.getAll();
+    }
+}
